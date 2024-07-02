5ebda39194abeb4ab05ec8008aaa38788e7e789b
J Hancock <123655328+J-A-Ha@users.noreply.github.com>
2024-07-02 14:52:29 +0100

    improving __init__

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 from .importers.crossref import lookup_doi, lookup_dois, lookup_journal, lookup_journals, search_journals, get_journal_entries, search_journal_entries, lookup_funder, lookup_funders, search_funders, get_funder_works, search_funder_works
 from .importers.crossref import search_works as search_crossref
-from .classes import Results, References, Author, Authors, Review
+from .classes import Results, References, Author, Authors, Review, Funder, Funders, Affiliation, Affiliations
