b64b5615e550a26d1a1672b98e9c88c5acff0eb0
J Hancock <123655328+J-A-Ha@users.noreply.github.com>
2024-07-09 18:45:46 +0100

    fixing bug in extract_referemces

--- classes/references.py
+++ classes/references.py
@@ -70,7 +70,7 @@ def extract_references(references_list, add_work_ids = True, update_from_doi = F
     if (type(references_list) != References) and (type(references_list) != str) and (type(references_list) != list):
         return refs
 
-    if (type(references_list) == list) and (type(references_list[0]) == dict):
+    if (type(references_list) == list) and (len(references_list) > 0) and (type(references_list[0]) == dict):
         df = references_to_df(references_list, update_from_doi = update_from_doi)
         df.replace({np.nan: None})
         refs = References.from_dataframe(df) # type: ignore
@@ -80,7 +80,7 @@ def extract_references(references_list, add_work_ids = True, update_from_doi = F
 
         return refs
     
-    if (type(references_list) == list) and (type(references_list[0]) == str):
+    if (type(references_list) == list) and (len(references_list) > 0) and (type(references_list[0]) == str):
         df = pd.DataFrame(columns=results_cols, dtype=object)
         df['link'] = pd.Series(references_list, dtype=object)
         df.replace({np.nan: None})
