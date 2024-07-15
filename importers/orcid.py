d74c92ad53f02e7e0bc5dfbbce6176308a0e425e
J Hancock <123655328+J-A-Ha@users.noreply.github.com>
2024-07-15 14:01:27 +0100

    adding get_author_works and get_author

--- importers/orcid.py
+++ importers/orcid.py
@@ -8,12 +8,16 @@ from time import sleep
 
 from requests import RequestException
 import pyorcid # type: ignore
+from pyorcid import OrcidAuthentication, Orcid # type: ignore
+
 import pandas as pd
 import numpy as np
 
 client_id = 'APP-105SVMUPF0B5U042'
 client_secret = 'c7455d6f-f546-4d01-af46-2c314a4a8f46'
-public_access_token = '00afb570-0432-4435-a8ca-00ef1063606c'
+orcid_auth = OrcidAuthentication(client_id=client_id, client_secret=client_secret)
+
+public_access_token = orcid_auth.get_public_access_token()
 
 def lookup_orcid(orcid_id = 'request_input'):
 
@@ -34,3 +38,34 @@ def lookup_orcid(orcid_id = 'request_input'):
                           dtype=object)
 
     return df
+
+def get_author(orcid_id = 'request_input'):
+
+    if orcid_id == 'request_input':
+        orcid_id = input('ORCID ID: ')
+    
+    global public_access_token
+    orcid = Orcid(orcid_id=orcid_id, orcid_access_token=public_access_token, state = "public")
+
+    return orcid
+
+def get_author_works(orcid_id = 'request_input', output = 'dataframe'):
+
+    if orcid_id == 'request_input':
+        orcid_id = input('ORCID ID: ')
+    
+    orcid = get_author(orcid_id=orcid_id)
+    works_tuple = orcid.works()
+    if len(works_tuple) > 0:
+        works_list = works_tuple[0]
+    else:
+        works_list = []
+
+    if (output == tuple) or (output == 'tuple'):
+        return works_tuple
+
+    if (output == list) or (output == 'list'):
+        return works_list
+    
+    if output == 'dataframe':
+        return pd.DataFrame(works_list)
