4edc142d0c3fef9d6b717240ff9fcf89749dc692
J Hancock <123655328+J-A-Ha@users.noreply.github.com>
2024-07-15 12:16:11 +0100

    improving __init__

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
+
 from .importers.crossref import lookup_doi, lookup_dois, lookup_journal, lookup_journals, search_journals, get_journal_entries, search_journal_entries, lookup_funder, lookup_funders, search_funders, get_funder_works, search_funder_works
 from .importers.crossref import search_works as search_crossref
-from .classes import Results, References, Author, Authors, Review, Funder, Funders, Affiliation, Affiliations
+from .importers.orcid import lookup_orcid
+from .importers import pdf, orcid, crossref, jstor
+from .classes import Results, References, Author, Authors, Funder, Funders, Affiliation, Affiliations, Review
+from .classes.networks import Network, Networks
 from .classes.citation_crawler import academic_scraper as scrape
