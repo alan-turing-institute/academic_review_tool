e6db0badc6dbec1c48b87d8606b743a9013da33c
J Hancock <123655328+J-A-Ha@users.noreply.github.com>
2024-07-16 10:59:52 +0100

    improving pybliometrics integration

--- importers/scopus.py
+++ importers/scopus.py
@@ -7,4 +7,4 @@ api_key = 'e015290bc75d27a1814cde5c468523e7'
 
 import pybliometrics # type: ignore
     
-pybliometrics.scopus.init(keys=[api_key])
+pybliometrics.scopus.create_config(keys = [api_key])
