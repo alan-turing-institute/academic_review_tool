7fb494089e3b29aa2f1a04a4aef4151a49ee78bd
J Hancock <123655328+J-A-Ha@users.noreply.github.com>
2024-07-16 10:48:47 +0100

    adding scopus.py

---
+++ importers/scopus.py
@@ -0,0 +1,10 @@
+from ..utils.basics import results_cols
+
+import pandas as pd
+import numpy as np
+
+api_key = 'e015290bc75d27a1814cde5c468523e7'
+
+import pybliometrics # type: ignore
+    
+pybliometrics.scopus.init(keys=[api_key])
