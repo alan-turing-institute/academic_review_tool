104cf230cb4ccce6d2e68c3c99ccdf9050d3071c
J Hancock <123655328+J-A-Ha@users.noreply.github.com>
2024-07-16 18:58:43 +0100

    updating __init__

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 
 from .importers.crossref import lookup_doi, lookup_dois, lookup_journal, lookup_journals, search_journals, get_journal_entries, search_journal_entries, lookup_funder, lookup_funders, search_funders, get_funder_works, search_funder_works
 from .importers.crossref import search_works as search_crossref
-from .importers.scopus import search as search_scopus
+from .importers.scopus import search as search_scopus, lookup as lookup_scopus
 from .importers.orcid import lookup_orcid, search as search_orcid
 from .importers import pdf, orcid, crossref, scopus, jstor
 from .classes import Results, References, Author, Authors, Funder, Funders, Affiliation, Affiliations, Review
