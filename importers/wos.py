8b39a552a439a3d537462f38138782d104a25f92
J Hancock <123655328+J-A-Ha@users.noreply.github.com>
2024-07-17 10:55:48 +0100

    adding search function

--- importers/wos.py
+++ importers/wos.py
@@ -4,10 +4,14 @@ import os
 import time
 from pprint import pprint
 
+import pandas as pd
+import numpy as np
+
 import metaknowledge as mk
 
 import clarivate.wos_starter.client as wos_client # type: ignore
 from clarivate.wos_starter.client.rest import ApiException # type: ignore
+from clarivate.wos_starter.client.models.documents_list import DocumentsList
 
 
 
@@ -56,3 +60,51 @@ def search_engine(query: str = 'request_input',
 
         except ApiException as e:
             print("Exception when calling DocumentsApi->documents_get: %s\n" % e)
+
+def search(query: str = 'request_input', 
+           database: str = 'WOK',
+           limit: int = 10,
+           page: int = 1,
+           sort_field: str = 'RS+D',
+           modified_time_span = None,
+           tc_modified_time_span = None,
+           detail = None
+           ):
+    
+    api_response = search_engine(query=query, 
+           database=database,
+           limit=limit,
+           page=page,
+           sort_field=sort_field,
+           modified_time_span = modified_time_span,
+           tc_modified_time_span = tc_modified_time_span,
+           detail = detail
+           )
+    
+    if (api_response is not None) and (type(api_response) == DocumentsList):
+
+        res_dict = api_response.to_dict()
+        meta = res_dict['metadata']
+        found = meta['total']
+        lim = meta['limit']
+        page_num = meta['page']
+        print(f'{found} results found. {lim} results returned from page {page_num}') # type: ignore
+
+        df = pd.DataFrame(res_dict['hits'], dtype=object)
+        df = df.rename(columns={
+                                'uid': 'wos_id',
+                                'types': 'type',
+                                'names': 'authors',
+                                'links': 'link'
+                            })
+        
+        for c in results_cols:
+            if c not in df.columns:
+                df[c] = pd.Series(dtype=object)
+        
+        df = df.replace(np.nan, None)
+    
+    else:
+        df = pd.DataFrame(columns=results_cols, dtype=object)
+    
+    return df
