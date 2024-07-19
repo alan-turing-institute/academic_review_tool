ec86d2b84c14afb36950b18793e73088e35f104a
J Hancock <123655328+J-A-Ha@users.noreply.github.com>
2024-07-19 15:12:02 +0100

    improving import_metadata

--- importers/jstor.py
+++ importers/jstor.py
@@ -10,46 +10,59 @@ import numpy as np
 def access_jstor_database():
     return webbrowser.open('https://constellate.org/builder')
 
-def import_jstor_metadata(file_path = 'request_input', clean_results = True):
+def import_metadata(file_path = 'request_input',
+                        #   clean_results = True
+                          ):
     
     if file_path == 'request_input':
         file_path = input('File path: ')
 
-    df = pd.read_csv(file_path, dtype=str).replace(np.nan, None)
+    import_df = pd.read_csv(file_path, dtype=object).replace(np.nan, None)
+    import_df = import_df.rename(columns=
+                                        {'id': 'other_ids',
+                                         'isPartOf': 'source',
+                                        'datePublished': 'date',
+                                        'docType': 'type',
+                                        'provider': 'repository',
+                                        'url': 'link',
+                                        'creator': 'authors',
+                                        'keyphrase': 'keywords'
+                                            })
+    
+    import_df = import_df.dropna(axis='columns', how='all')
 
     global results_cols
-    output_df = pd.DataFrame(columns=results_cols)
+
+    to_drop = []
+    for c in import_df.columns:
+        if c not in results_cols:
+            to_drop.append(c)
     
-    output_df['title'] = df['title'].str.lower()
-    output_df['link'] = df['id']
-    output_df['source'] = df['isPartOf']
-    output_df['date'] = df['publicationdate']
-    output_df['doi'] = df['doi']
-    output_df['type'] = df['docType']
-    output_df['repository'] = df['provider']
-    output_df['authors'] = df['creator'].str.lower().str.split(';')
-    output_df['publisher'] = df['publisher']
-    output_df['keywords'] = df['keyphrase'].str.lower().str.split(';')
+
+
+    output_df = import_df.drop(labels=to_drop, axis='columns')
+    output_df['authors'] = output_df['authors'].str.split(';')
+    output_df['keywords'] = output_df['keywords'].str.lower().str.split(';')
 
     output_df = output_df.replace(np.nan, None).replace('untitled', None)
     
     
-    if clean_results == True:
+    # if clean_results == True:
         
-        cleaning_list = ['book review:',
-                        '\[',
-                        'references',
-                         'index'
-                        ]
+    #     cleaning_list = ['book review:',
+    #                     '\[',
+    #                     'references',
+    #                      'index'
+    #                     ]
 
-        for item in cleaning_list:
-            instances = output_df[output_df['title'].str.contains(item) == True].index.to_list()
-            for i in instances:
-                output_df.loc[i, 'title'] = None
+    #     for item in cleaning_list:
+    #         instances = output_df[output_df['title'].str.contains(item) == True].index.to_list()
+    #         for i in instances:
+    #             output_df.loc[i, 'title'] = None
     
     return output_df
 
-def import_jstor_full(file_path = 'request_input', clean_results = True):
+def import_full(file_path = 'request_input', clean_results = True):
     
     if file_path == 'request_input':
         file_path = input('File path: ')
