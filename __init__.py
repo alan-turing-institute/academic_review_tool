a102255ca6a840bafb359e24600de257413e8412
J Hancock <123655328+J-A-Ha@users.noreply.github.com>
2024-08-02 18:03:39 +0100

    adding open_file function

--- __init__.py
+++ __init__.py
@@ -9,3 +9,16 @@ from .importers import pdf, orcid, crossref, scopus, jstor, wos
 from .classes import Results, References, Author, Authors, Funder, Funders, Affiliation, Affiliations, Review
 from .classes.networks import Network, Networks
 from .classes.citation_crawler import academic_scraper as scrape
+
+import pickle
+
+ def open_file(file_address: str = 'request_input'): # type: ignore
+
+        if file_address == 'request_input':
+            file_address = input('File address: ')
+        
+        if (file_address.endswith('.txt')) or (file_address.endswith('.review')):
+            with open(file_address, 'rb') as f:
+                review = pickle.load(f)
+        
+        return review
