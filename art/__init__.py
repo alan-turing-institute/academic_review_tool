db9e50e586d59c88a69ab3e2c081e549893de7f8
J Hancock <123655328+J-A-Ha@users.noreply.github.com>
2024-09-21 15:03:28 +0100

    updating pyproject.toml, SECURITY.md, and __init__.py

--- art/__init__.py
+++ art/__init__.py
@@ -28,6 +28,8 @@ ART uses the following APIs:
 * Scopus
 * ORCID
 * Geopy / Nominatim
+
+Version: 1.1.0
 """
 
 from .importers.crossref import lookup_doi, lookup_dois, lookup_journal, lookup_journals, search_journals, get_journal_entries, search_journal_entries, lookup_funder, lookup_funders, search_funders, get_funder_works, search_funder_works
